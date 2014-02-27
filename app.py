5bffa58dcdf11ac9aca37d68fd92d2b77ae45d80
Hackbright Student <user@mini-004.(none)>
2014-02-26 18:06:20 -0800

    Authenticates username and password.

--- app.py
+++ app.py
@@ -6,15 +6,43 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in!" %session["username"]
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    status = model.authenticate(username, password)
+    if status == "SUCCESS":
+        flash("User authenticated")
+        session['username'] = username
+        return redirect(url_for("index"))    
+    elif status == "no_user":
+        flash("User does not exist")
+        return redirect(url_for("index"))
+    elif status == "incorrect":
+        flash("Username or Password incorrect, there may be a ferret stampede in progress!")
+        return redirect(url_for("index"))
+    
+
+@app.route("/user/<username>")
+def view_user(username):
+    model.get_user_by_name("username")
+    # show the user profile for that user
+    return 'User %s is logged in.' % username
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,38 @@
+import sqlite3
+
+DB = None
+CONN = None
+
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
 
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
+def get_user_by_name(username):
+    query = """SELECT user_id FROM Users WHERE username = ?"""
+    DB.execute(query, (username,))
+    user_id = DB.fetchone()
+    return user_id
+
 def authenticate(username, password):
-    return False
+    connect_to_db()
+    query = """SELECT password FROM Users WHERE username = ?"""
+    DB.execute(query, (username,))
+    DBpassword = DB.fetchone()
+
+
+
+    if DBpassword == None:
+        print "no_user" 
+        return "no_user"
+    elif int(DBpassword[0]) == hash(password):
+        return "SUCCESS"
+    else:
+        print "you suck"
+        return "incorrect"
+
+
+
